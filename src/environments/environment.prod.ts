2878dea019bf379c64306c1bcfcbf535f0f68d0c
Sammed <sammed.patil29@gmail.com>
2026-04-12 20:24:32 +0530

    prod api updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://may-i-help-you-foundation-backend-hlcs5p-c8ede0-203-57-85-153.traefik.me'
+  apiUrl: 'https://ngo-api.democompany.in.net'
 };
