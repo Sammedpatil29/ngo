9e442247882ace5357e450522daf2b2a3756a2e7
Sammed <sammed.patil29@gmail.com>
2026-04-12 16:57:08 +0530

    url canges

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'may-i-help-you-foundation-backend-hlcs5p-04ec15-203-57-85-153.traefik.me'
+  apiUrl: 'http://may-i-help-you-foundation-backend-hlcs5p-04ec15-203-57-85-153.traefik.me'
 };
